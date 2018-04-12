93082a70d9079f6fe8656d88a52ee99d8971ab8a
im0qianqian <qian1335661317@qq.com>
2018-04-12 19:12:09 +0800

    init

---
+++ CodeforcesAPI/Codeforces.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeforcesAPI
+{
+    public class Codeforces
+    {
+        public static string Test()
+        {
+            return "Hello World!";
+        }
+    }
+}
