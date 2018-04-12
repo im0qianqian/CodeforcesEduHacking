309fecabfe39c50ffea69d75333ff13ae95d3169
im0qianqian <qian1335661317@qq.com>
2018-04-12 20:25:46 +0800

    init

--- CodeforcesEduHacking/MainForm.cs
+++ CodeforcesEduHacking/MainForm.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CodeforcesAPI;
 
 namespace CodeforcesEduHacking
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            label1.Text = Codeforces.Test();
         }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using CodeforcesAPI;
 
-namespace Test
+namespace TestConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string test = Codeforces.Test();
-            Console.WriteLine(test);
+            Console.WriteLine(Codeforces.Test());
         }
     }
 }
